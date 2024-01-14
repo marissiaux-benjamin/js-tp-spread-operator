ea2de51661822aacc699544948956da50fb4ac94
benjamin <benjamin.marissiaux@student.hepl.be>
2024-01-14 14:45:47 +0100

    js-tp-spread-operator

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,3 @@
-/*document.documentElement.classList.remove('no-js');
-document.documentElement.classList.add('js-enabled');
-
-const jumpElement = document.querySelector('.jump');
-const jumpText = jumpElement.textContent;
-
-for (let i = 0; i < 24; i++) {
-    let tableau = jumpText[i];
-    jumpElement.insertAdjacentHTML('beforeend',`<span>${tableau}</span`);
-}*/
-
 
 document.querySelector('html').classList.add('js-enabled');
 
