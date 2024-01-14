7516359168be7e907af7978513db068e258deff1
benjamin <benjamin.marissiaux@student.hepl.be>
2024-01-14 14:45:22 +0100

    js-tp-spread-operator

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,61 @@
-document.documentElement.classList.remove('no-js');
+/*document.documentElement.classList.remove('no-js');
 document.documentElement.classList.add('js-enabled');
 
 const jumpElement = document.querySelector('.jump');
-const jumpText = jumpElement.textContent
+const jumpText = jumpElement.textContent;
 
 for (let i = 0; i < 24; i++) {
     let tableau = jumpText[i];
     jumpElement.insertAdjacentHTML('beforeend',`<span>${tableau}</span`);
+}*/
+
+
+document.querySelector('html').classList.add('js-enabled');
+
+const h1Selector = document.querySelector('.jump');
+const lettresSeparees = h1Selector.textContent;
+function removeH1 (){
+    h1Selector.textContent = '';
 }
+removeH1();
+
+for (const lettre of lettresSeparees) {
+    const nbsp = '\u00A0';
+    if (lettre === lettre){
+    h1Selector.insertAdjacentHTML('beforeend', `<span>${lettre}</span>` );
+    }
+    if (lettre === ' '){
+        h1Selector.insertAdjacentHTML('beforeend', `<span>${nbsp}</span>`);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
